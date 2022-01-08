e299def8ac086f7eaf23f027af135d454a5eba75
Raza Hassan <bilgramiraza@yahoo.com>
2022-01-08 19:27:36 +0530

    added main class component for details

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
+import React, { Component } from 'react'
+import Display from './components/Display'
+import InputSection from './components/InputSection';
+export default class App extends Component {
+  constructor(){
+    super();
 
-function App() {
-  return (
-    <div>
-      Clean Slate
-    </div>
-  );
-}
+    this.state={
+      personalInfo:{
+        fullName:'',
+        email:'',
+        location:'',
+        phoneNo:'',
+      },
+      education:[{
+        instituteName:'',
+        location:'',
+        qualification:'',
+        duration:{
+          from:'',
+          to:'',
+        },
+      }],
+      experience:[{
+        companyName:'',
+        title:'',
+        location:'',
+        duration:{
+          from:'',
+          to:'',
+        },
+      }],
+    };
+  }
+  getData=(data)=>{
 
-export default App;
+  };
+  render() {
+    const {personalInfo, education, experience}=this.state;
+    return (
+      <div>
+        <InputSection getData={this.getData}/>
+        <Display personalInfo={personalInfo} education={education} experience={experience} />
+      </div>
+    )
+  }
+};

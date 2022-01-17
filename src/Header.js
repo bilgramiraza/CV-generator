83092b4632111df4657521ccea29cc06690b7f4f
Raza Hassan <bilgramiraza@yahoo.com>
2022-01-17 16:40:24 +0530

    Created basic header/footer section

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+    render() {
+        return (
+            <header>
+                <img src='#' alt='Logo'/>
+                <h1>CV Generator</h1>
+            </header>
+        )
+    }
+}

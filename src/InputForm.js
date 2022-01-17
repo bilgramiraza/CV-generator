b6b5f1202a9325a6a672a45b8831e66232b24c4f
Raza Hassan <bilgramiraza@yahoo.com>
2022-01-17 16:43:55 +0530

    setup Input section with personal Info

---
+++ src/InputForm.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import InputPersonal from './InputPersonal';
+import InputEducation from './InputEducation';
+
+export default class InputForm extends Component {
+    handleChange(category,e){
+        this.props.getData(category,e.target.name,e.target.value);
+    }
+    render() {
+        const {personal, education, experience} = this.props.data;
+        return (
+            <div className='container'>
+                <h3>Personal Info</h3>
+                <InputPersonal handleChange={(e)=>this.handleChange('personal',e)} data={personal}/>
+                <hr />
+                <h3>Education Info</h3>
+                <InputEducation handleChange={(e)=>this.handleChange('education',e)} data={education}/>
+            </div>
+        )
+    }
+}

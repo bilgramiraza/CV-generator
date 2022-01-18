3a1db168c95549f16c8e41726dc4cc01769dd2dd
Raza Hassan <bilgramiraza@yahoo.com>
2022-01-18 13:20:32 +0530

    Setup state to collect Education details

--- src/Main.js
+++ src/Main.js
@@ -13,25 +13,85 @@ export default class Main extends Component {
                 location:'',
                 mobileNo:'',
             },
-            education:[],
+            education:{
+                tracker:0,
+                history:[{
+                    instituteName:'',
+                    qual:'',
+                    location:'',
+                    from:'',
+                    to:'',
+                }],
+            },
             experience:[],
         };
     }
     getData(category,name,value){
-        this.setState(prevState=>({
+        switch (category) {
+            case "personal":
+                this.setState(prevState=>({
+                    ...prevState,
+                    [category]:{
+                        ...prevState[category],
+                        [name]:value,
+                    },
+                }));
+                break;
+        
+            default:
+                const target = this.state[category].history;
+                target[this.state[category].tracker]={
+                    ...target[this.state[category].tracker],
+                    [name]:value,
+                };
+                this.setState(prevState=>({
+                    ...prevState,
+                    [category]:{
+                        ...prevState[category],
+                        history:[...target],
+                    }
+                }));
+                break;
+        }
+    }
+    handleTracker(category, step){
+        const categoryLength=this.state[category].history.length;
+
+        if((this.state[category].tracker+step) >= categoryLength || 
+           (this.state[category].tracker+step) < 0)
+            return;
+
+        this.setState((prevState)=>({
             ...prevState,
             [category]:{
                 ...prevState[category],
-                [name]:value,
-            },
+                tracker:prevState[category].tracker+step,
+            }
         }));
     }
     render() {
         return (
             <main>
-                <InputForm getData={this.getData.bind(this)} data={this.state}/>
+                <InputForm getData={this.getData.bind(this)} 
+                handleTracker={this.handleTracker.bind(this)} 
+                data={this.state}/>
                 <Display data={this.state}/>
             </main>
         )
     }
 }
+
+
+// this.setState((prevState)=>({
+//     ...prevState,
+//     [category]:{
+//         tracker:prevState[category].tracker+step,
+//         history:[...prevState[category].history, {
+//             instituteName:'',
+//             qual:'',
+//             location:'',
+//             from:'',
+//             to:'',
+//         }],
+//     }
+// }));

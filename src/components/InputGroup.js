fecc169e29237fd95313df80114f605e23c8b827
Raza Hassan <bilgramiraza@yahoo.com>
2022-01-30 13:23:33 +0530

    Added Required attributes to form Builder

--- src/components/InputGroup.js
+++ src/components/InputGroup.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 export default class InputGroup extends Component {
     render() {
-        const {inputGroupLabel, inputGroupType, inputGroupName, inputGroupValue, handleChange} =this.props;
+        const {inputGroupLabel, inputGroupType, inputGroupName, inputGroupValue, handleChange, required} =this.props;
         return (
             <>
                 <span className="input-group-text">{inputGroupLabel}</span>
@@ -11,7 +11,8 @@ export default class InputGroup extends Component {
                     aria-label={inputGroupLabel}
                     name={inputGroupName}
                     value={inputGroupValue}
-                    onChange={handleChange}/>
+                    onChange={handleChange}
+                    required={required?'required':''}/>
             </>
         )
     }

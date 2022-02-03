fd525889d28c6b9c3d5949d6146c92411d4131c8
Raza Hassan <bilgramiraza@yahoo.com>
2022-02-03 11:30:51 +0530

    Added Ability to generate a textarea input field

--- src/components/InputGroup.js
+++ src/components/InputGroup.js
@@ -2,18 +2,38 @@ import React, { Component } from 'react'
 
 export default class InputGroup extends Component {
     render() {
-        const {inputGroupLabel, inputGroupType, inputGroupName, inputGroupValue, handleChange, required} =this.props;
-        return (
-            <>
-                <span className="input-group-text">{inputGroupLabel}</span>
-                <input type={inputGroupType}
-                    className='form-control'
-                    aria-label={inputGroupLabel}
-                    name={inputGroupName}
-                    value={inputGroupValue}
-                    onChange={handleChange}
-                    required={required?'required':''}/>
-            </>
-        )
+        const {inputGroupLabel, inputGroupType, inputGroupName, 
+               inputGroupValue, pattern, handleChange, required} =this.props;
+        switch (inputGroupType) {
+            case 'textarea':
+                return (
+                    <>
+                        <span className="input-group-text">{inputGroupLabel}</span>
+                        <textarea
+                            className='form-control'
+                            aria-label={inputGroupLabel}
+                            name={inputGroupName}
+                            value={inputGroupValue}
+                            pattern={pattern?pattern:null}
+                            onChange={handleChange}
+                            required={required?'required':null}/>
+                    </>
+                );
+        
+            default:
+                return (
+                    <>
+                        <span className="input-group-text">{inputGroupLabel}</span>
+                        <input type={inputGroupType}
+                            className='form-control'
+                            aria-label={inputGroupLabel}
+                            name={inputGroupName}
+                            value={inputGroupValue}
+                            pattern={pattern?pattern:null}
+                            onChange={handleChange}
+                            required={required?'required':null}/>
+                    </>
+                );
+        }
     }
 }

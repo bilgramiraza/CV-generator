13b81d33553a30c954224b17feea8be1da5d7d75
Raza Hassan <bilgramiraza@yahoo.com>
2022-02-06 13:30:54 +0530

    Setup layout of Header and Footer components

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,10 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class Header extends Component {
     render() {
         return (
             <header>
-                <img src='#' alt='Logo'/>
-                <h1>CV Generator</h1>
+                <h1 className='mx-auto'>CV Generator</h1>
             </header>
         )
     }

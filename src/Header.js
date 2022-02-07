ee24ee1f4db3a976ddd10a5585cff30c45275264
Raza Hassan <bilgramiraza@yahoo.com>
2022-02-07 08:57:58 +0530

    Fixed Minor Spelling mistakes

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ export default class Header extends Component {
     render() {
         return (
             <header>
-                <h1 className='mx-auto'>CV Generator</h1>
+                <h1 className='m-auto'>CV Generator</h1>
             </header>
         )
     }

cf2496556b3d41a043e95e9f98a6e3710621e336
bilgramiraza <bilgramiraza@yahoo.com>
2022-07-16 12:47:31 +0530

    converted from 4-2 spaces and from class to functional components

--- src/components/InputCarouselSelector.js
+++ src/components/InputCarouselSelector.js
@@ -1,45 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class InputCarouselSelector extends Component {
-    handleChange=(e)=>{
-        this.props.handleTracker(this.props.category,parseInt(e.target.value))
-    }
-    incrementTracker=(e)=>{
-        e.preventDefault();
-        const changeValue = this.props.currentIndex+1;
-        if(changeValue<this.props.totalItems)
-            this.props.handleTracker(this.props.category, changeValue);
-    }
-    decrementTracker=(e)=>{
-        e.preventDefault();
-        const changeValue = this.props.currentIndex-1;
-        if(changeValue>=0)
-            this.props.handleTracker(this.props.category, changeValue);
-    }
-  render() {
-      const inputIndicators = [...Array(this.props.totalItems)].map((div,i)=>{
-                                return (
-                                    <li className={`page-item ${i===this.props.currentIndex?'active':''}`}
-                                        key={i}>
-                                        <button className='page-link'
-                                                onClick={this.handleChange}
-                                                value={i}>
-                                            {i+1}
-                                        </button>
-                                    </li>);
-                                });
+export default function InputCarouselSelector(props) {
+  const handleChange = (e) => {
+    props.handleTracker(props.category, parseInt(e.target.value))
+  };
+  const incrementTracker = (e) => {
+    e.preventDefault();
+    const changeValue = props.currentIndex + 1;
+    if (changeValue < props.totalItems)
+      props.handleTracker(props.category, changeValue);
+  };
+  const decrementTracker = (e) => {
+    e.preventDefault();
+    const changeValue = props.currentIndex - 1;
+    if (changeValue >= 0)
+      props.handleTracker(props.category, changeValue);
+  };
+  const inputIndicators = [...Array(props.totalItems)].map((div, i) => {
     return (
-        <nav>
-        <ul className="my-auto pagination justify-content-center">
-            <li className="page-item">
-                <button className="page-link" onClick={this.decrementTracker}>Previous</button>
-            </li>
-            {inputIndicators}
-            <li className="page-item">
-                <button className="page-link" onClick={this.incrementTracker}>Next</button>
-            </li>
-        </ul>
+      <li className={`page-item ${i === props.currentIndex ? 'active' : ''}`}
+        key={i}>
+        <button className='page-link'
+          onClick={handleChange}
+          value={i}>
+          {i + 1}
+        </button>
+      </li>);
+  });
+  return (
+    <nav>
+      <ul className="my-auto pagination justify-content-center">
+        <li className="page-item">
+          <button className="page-link" onClick={decrementTracker}>Previous</button>
+        </li>
+        {inputIndicators}
+        <li className="page-item">
+          <button className="page-link" onClick={incrementTracker}>Next</button>
+        </li>
+      </ul>
     </nav>
-    );
-  }
+  );
 }

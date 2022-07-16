4e678b897a49587f82e5267107a9d8ea1c9cdfb8
bilgramiraza <bilgramiraza@yahoo.com>
2022-07-16 12:44:32 +0530

    converted from 4-2 spaces and from class to functional components

--- src/components/InputGroup.js
+++ src/components/InputGroup.js
@@ -1,41 +1,39 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class InputGroup extends Component {
-    render() {
-        const {inputGroupLabel, inputGroupType, inputGroupName, inputGroupPlaceHolder,
-               inputGroupValue, pattern, handleChange, required} =this.props;
-        switch (inputGroupType) {
-            case 'textarea':
-                return (
-                    <>
-                        <span className="input-group-text">{inputGroupLabel}</span>
-                        <textarea
-                            className='form-control'
-                            aria-label={inputGroupLabel}
-                            name={inputGroupName}
-                            placeholder={inputGroupPlaceHolder?inputGroupPlaceHolder:null}
-                            value={inputGroupValue}
-                            pattern={pattern?pattern:null}
-                            onChange={handleChange}
-                            required={required?'required':null}/>
-                    </>
-                );
-        
-            default:
-                return (
-                    <>
-                        <span className="input-group-text">{inputGroupLabel}</span>
-                        <input type={inputGroupType}
-                            className='form-control'
-                            aria-label={inputGroupLabel}
-                            name={inputGroupName}
-                            placeholder={inputGroupPlaceHolder?inputGroupPlaceHolder:null}
-                            value={inputGroupValue}
-                            pattern={pattern?pattern:null}
-                            onChange={handleChange}
-                            required={required?'required':null}/>
-                    </>
-                );
-        }
-    }
+export default function InputGroup(props) {
+  const { inputGroupLabel, inputGroupType, inputGroupName, inputGroupPlaceHolder,
+    inputGroupValue, pattern, handleChange, required } = props;
+  switch (inputGroupType) {
+    case 'textarea':
+      return (
+        <>
+          <span className="input-group-text">{inputGroupLabel}</span>
+          <textarea
+            className='form-control'
+            aria-label={inputGroupLabel}
+            name={inputGroupName}
+            placeholder={inputGroupPlaceHolder ? inputGroupPlaceHolder : null}
+            value={inputGroupValue}
+            pattern={pattern ? pattern : null}
+            onChange={handleChange}
+            required={required ? 'required' : null} />
+        </>
+      );
+
+    default:
+      return (
+        <>
+          <span className="input-group-text">{inputGroupLabel}</span>
+          <input type={inputGroupType}
+            className='form-control'
+            aria-label={inputGroupLabel}
+            name={inputGroupName}
+            placeholder={inputGroupPlaceHolder ? inputGroupPlaceHolder : null}
+            value={inputGroupValue}
+            pattern={pattern ? pattern : null}
+            onChange={handleChange}
+            required={required ? 'required' : null} />
+        </>
+      );
+  }
 }
